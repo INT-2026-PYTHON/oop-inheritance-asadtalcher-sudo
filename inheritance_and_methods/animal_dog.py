58c054e29f292cbfdffa082671a76534ebcb4949
asadtalcher-sudo <asadtalcher@gmail.com>
2026-06-12 16:50:08 +0530

    Update animal_dog.py

--- inheritance_and_methods/animal_dog.py
+++ inheritance_and_methods/animal_dog.py
@@ -67,3 +67,29 @@ Rex is a Beagle
 =================================================
 
 """
+#25BCSG20 ASAD AHMAD
+# Parent class
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+
+    def speak(self):
+        print(f"{self.name} says {self.sound}")
+
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name, "Woof")
+        self.breed = breed
+    def describe(self):
+        print(f"{self.name} is a {self.breed}")
+if __name__ == "__main__":
+    a = Animal("Cat", "Meow")
+    a.speak() 
+    d1 = Dog("Buddy", "Labrador")
+    d2 = Dog("Rex", "Beagle")
+    d1.speak()        
+    d1.describe()      
+    d2.speak()         
+    d2.describe()      
